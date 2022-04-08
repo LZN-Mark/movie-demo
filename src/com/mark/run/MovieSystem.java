d043a6c457b8543d02008b92d5e37c2275edf0dd
mark <llzznn2012llzznn@126.com>
2022-04-08 22:02:52 +0800

    影片下架、修改、展示排片信息、用户购票功能实现

--- src/com/mark/run/MovieSystem.java
+++ src/com/mark/run/MovieSystem.java
@@ -7,6 +7,7 @@ import com.mark.pojo.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -180,9 +181,11 @@ public class MovieSystem {
                     break;
                 case "3":
                     // 下架电影信息
+                    deleteMovie();
                     break;
                 case "4":
                     // 修改电影信息
+                    updateMovie();
                     break;
                 case "5":
                     System.out.println(loginUser.getUserName() +"请您下次再来啊~~~");
@@ -195,9 +198,93 @@ public class MovieSystem {
     }
 
     /**
+     * 影片修改功能
+     */
+    private static void updateMovie() {
+        System.out.println("===================影片修改=================");
+        Business business = (Business) loginUser;
+        List<Movie> movies = ALL_MOVIES.get(business);
+        if(movies.size() == 0){
+            System.out.println("当前无片可以修改");
+            return;
+        }else{
+            //2.让用户选择需要修改的电影名称
+            while (true) {
+                System.out.println("请输入您想修改的电影名称");
+                String movieName = SYS_SC.nextLine();
+                for (Movie movie : movies) {
+                    if (movie.getName().equals(movieName)){
+                        //该影片存在
+                        System.out.println("请您输入修改后的电影名:");
+                        String name = SYS_SC.nextLine();
+                        System.out.println("请您输入修改后的主演:");
+                        String actor = SYS_SC.nextLine();
+                        System.out.println("请您输入修改后的时长:");
+                        String time = SYS_SC.nextLine();
+                        System.out.println("请您输入修改后的票价:");
+                        String price = SYS_SC.nextLine();
+                        System.out.println("请您输入修改后的票数:");
+                        String totalNumber = SYS_SC.nextLine();
+                        while (true) {
+                            try {
+                                System.out.println("请您输入修改后的影片放映时间:");
+                                String startTime = SYS_SC.nextLine();
+                                movie.setName(name);
+                                movie.setActor(actor);
+                                movie.setTime(Double.valueOf(time));
+                                movie.setPrice(Double.valueOf(price));
+                                movie.setNumber(Integer.valueOf(totalNumber));
+                                movie.setStartTime(sdf.parse(startTime));
+                                System.out.println("恭喜您，您成功修改了该影片");
+                                showBusinessInfos();
+                                return;//退出方法
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                LOGGER.error("时间解析出错");
+                                System.out.println("请重新输入影片放映时间");
+                            }
+                        }
+                    }
+                }
+                System.out.println("您要修改的影片不存在，请重新输入电影名");
+            }
+        }
+    }
+
+    /**
+     * 影片下架功能
+     */
+    private static void deleteMovie() {
+        System.out.println("===================影片下架=================");
+        Business business = (Business) loginUser;
+        List<Movie> movies = ALL_MOVIES.get(business);
+        if(movies.size() == 0){
+            System.out.println("当前无片可以下架");
+            return;
+        }else{
+            //2.让用户选择需要下架的电影名称
+            while (true) {
+                System.out.println("请输入您想下架的电影名称");
+                String movieName = SYS_SC.nextLine();
+                for (Movie movie : movies) {
+                    if (movie.getName().equals(movieName)){
+                        //该影片存在
+                        movies.remove(movie);
+                        System.out.println("下架成功");
+                        showBusinessInfos();
+                        return;
+                    }
+                }
+                System.out.println("您要下架的影片不存在，请重新输入电影名");
+            }
+        }
+    }
+
+    /**
      * 商家上架电影
      */
     private static void addMovie() {
+        System.out.println("=======================上架电影=====================");
         Business business = (Business) loginUser;
         List<Movie> movies = ALL_MOVIES.get(business);
         System.out.println("请您输入电影名:");
@@ -235,7 +322,7 @@ public class MovieSystem {
         LOGGER.info(loginUser.getLoginName()+"商家正在查看详情");
         //根据商家对象(loginUser就是登录的用户)，作为Map集合的键，提取对应的值就是排片信息：Map<Business,List<Movie>> ALL_MOVIES
         Business business = (Business) loginUser;
-        System.out.println(business.getShopName() + "\t\t电话" +business.getPhone() + "\t\t地址:" +business.getAddress());
+        System.out.println(business.getShopName() + "\t\t电话" +business.getPhone() + "\t\t地址:" +business.getAddress()+ "\t\t金额:" +business.getMoney());
         List<Movie> movies = ALL_MOVIES.get(loginUser);
         if (movies.size() > 0) {
             System.out.println("片名\t\t\t主演\t\t时长\t\t评分\t\t票价\t\t余票数量\t\t放映时间");
@@ -270,6 +357,7 @@ public class MovieSystem {
             switch (command){
                 case "1":
                     // 展示全部排片信息
+                    showAllMovies();
                     break;
                 case "2":
                     break;
@@ -278,6 +366,7 @@ public class MovieSystem {
                     break;
                 case "4":
                     // 购票功能
+                    buyMovie();
                     break;
                 case "5":
                     return; // 干掉方法
@@ -286,6 +375,148 @@ public class MovieSystem {
                     break;
             }
         }
+    }
+
+    /**
+     * 用户购票功能
+     */
+    private static void buyMovie() {
+        showAllMovies();
+        System.out.println("==========================用户购票功能=========================");
+        while (true) {
+            System.out.println("请您输入想要购票的门店：");
+            String shopName = SYS_SC.nextLine();
+            //1.查询是否存在该商家
+            Business business = getBusinessByShopName(shopName);
+            if (business == null){
+                //商家不存在
+                System.out.println("对不起，没有该店铺，请重新输入店铺名");
+            }else{
+                //商家存在，展示商家全部的排片
+                List<Movie> movies = ALL_MOVIES.get(business);
+                //判断是否有影片
+                if(movies.size() == 0){
+                    //无片
+                    System.out.println("该影院没有排片");
+                    System.out.println("是否继续买票？Y/N");
+                    String command = SYS_SC.nextLine();
+                    switch (command){
+                        case "Y":
+                            break;
+                        default:
+                            return;
+                    }
+                }else{
+                    //有片,并开始进行选片购买
+                    while (true) {
+                        System.out.println("请输入需要购买的电影名称：");
+                        String movieName = SYS_SC.nextLine();
+                        //去当前商家下，查询电影对象
+                        Movie m = getMovieByShopAndName(business, movieName);
+                        if(m != null){
+                            //有该电影，开始购买
+                            while (true) {
+                                System.out.println("请您输入想购买的电影票数");
+                                String number = SYS_SC.nextLine();
+                                Integer num = Integer.valueOf(number);
+                                //判断电影票是否足够
+                                if(m.getNumber() >= num){
+                                    //有票
+                                    //算出当前需要花费的金额
+                                    Double money = BigDecimal.valueOf(num).multiply(BigDecimal.valueOf(m.getPrice())).doubleValue();
+                                    System.out.println("您总共需要花费"+money+"元");
+                                    //判断钱够不够
+                                    if(loginUser.getMoney() >= money){
+                                        //钱够
+                                        while (true) {
+                                            System.out.println("您的余额充足，是否购买 Y/N");
+                                            String commad = SYS_SC.nextLine();
+                                            if("Y".equals(commad)){
+                                                System.out.println("您已经成功购买了《"+m.getName()+"》的电影票"+num+"张");
+                                                loginUser.setMoney(loginUser.getMoney() - money);
+                                                business.setMoney(business.getMoney() + money);
+                                                m.setNumber(m.getNumber() - num);
+                                                System.out.println("购买成功，您的余额还剩下"+loginUser.getMoney()+"元");
+                                                return;
+                                            }else if ("N".equals(commad)){
+                                                return;
+                                            }else {
+                                                System.out.println("您输入的指令有误，请确认");
+                                            }
+                                        }
+                                    }else{
+                                        //钱不够
+                                        System.out.println("抱歉，您的余额不足");
+                                        System.out.println("是否继续买票？ Y/N");
+                                        String commad = SYS_SC.nextLine();
+                                        switch (commad){
+                                            case "Y":
+                                                break;
+                                            default:
+                                                return;
+                                        }
+                                    }
+                                }else{
+                                    //没票
+                                    System.out.println("没那么多票,您当前最多可以购买"+m.getNumber()+"张");
+                                    System.out.println("是否继续买票？ Y/N");
+                                    String commad = SYS_SC.nextLine();
+                                    switch (commad){
+                                        case "Y":
+                                            break;
+                                        default:
+                                            return;
+                                    }
+                                }
+                            }
+
+                        }else{
+                            //没有该电影
+                            System.out.println("该商家没有该电影");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static Movie getMovieByShopAndName(Business business,String name){
+        List<Movie> movies = ALL_MOVIES.get(business);
+        for (Movie movie : movies) {
+            if (movie.getName(). contains(name)){
+                return movie;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 根据店铺名称寻找商家
+     * @return
+     */
+    public static Business getBusinessByShopName(String shopName){
+        Set<Business> businesses = ALL_MOVIES.keySet();
+        for (Business business : businesses) {
+            if (business.getShopName().equals(shopName)){
+                return business;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 用户功能:展示全部商家排片信息
+     */
+    private static void showAllMovies() {
+        System.out.println("=========================全部商家排片信息========================");
+        ALL_MOVIES.forEach((business, movies) -> {
+            System.out.println(business.getShopName() + "\t\t电话" +business.getPhone() + "\t\t地址:" +business.getAddress());
+            System.out.println("片名\t\t\t主演\t\t时长\t\t评分\t\t票价\t\t余票数量\t\t放映时间");
+            for (Movie movie : movies) {
+                System.out.println(movie.getName() + "\t\t\t" + movie.getActor() + "\t\t" + movie.getTime() + "\t\t"
+                        + movie.getScore() + "\t\t\t" + movie.getPrice() + "\t\t\t" + movie.getNumber() + "\t\t\t" + sdf.format(movie.getStartTime()));
+            }
+        });
     }
 
     private static User getUserByLoginName(String loginName) {
